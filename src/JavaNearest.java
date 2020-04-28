34981ee4d972c536939f389401f5789d737e189e
Jens <jenav16@student.sdu.dk>
2020-04-28 11:23:11 +0200

    Here b dragonz

--- src/JavaNearest.java
+++ src/JavaNearest.java
@@ -4,24 +4,55 @@ import java.util.Map;
 public class JavaNearest {
     public static void main(String[] args) {
         ReadFile readFileTest = new ReadFile("lib/experiment1/test", "data.csv");
-        //List<Observation> testObservations = readFileTest.getObservations();
         Map<Integer, List<Double>> testObservations = readFileTest.getObservations();
         ReadFile readFileTrain = new ReadFile("lib/experiment1/train", "data.csv");
         Map<Integer, List<Double>> trainObservations = readFileTest.getObservations();
-        
-        testObservations.forEach((integer, doubles) -> {
 
-            System.out.println(integer + " " + doubles);
+
+        for (Map.Entry<Integer, List<Double>> e : testObservations.entrySet()) {
+            Integer integer = e.getKey();
+            List<Double> doubles = e.getValue();
+
             double distance = 0.0;
-            for (Map.Entry<Integer, List<Double>> entry: trainObservations.entrySet()) {
-                for (int i = 0; i < entry.getValue().size(); i++){
-                    if(i%4 == 2){
-                        distance = Math.pow(doubles.get(i) - entry.getValue().get(i), 2);
+
+            //Here be dragons, plz refactor
+            for (Map.Entry<Integer, List<Double>> entry : trainObservations.entrySet()) {
+
+                for (int i = 0; i < entry.getValue().size(); i++) {
+                    if (entry.getValue().size() == doubles.size()) {
+                        if (i % 3 == 2) {
+                            distance = Math.pow(doubles.get(i) - entry.getValue().get(i), 2);
+                        }
+                    } else if (entry.getValue().size() > doubles.size()) {
+                        if (i > doubles.size()) {
+                            if (i % 3 == 2) {
+                                distance = Math.pow(entry.getValue().get(i), 2);
+                            }
+                        } else {
+                            if (i % 3 == 2) {
+                                distance = Math.pow(doubles.get(i) - entry.getValue().get(i), 2);
+                            }
+                        }
+                    } else if (entry.getValue().size() < doubles.size()) {
+                        if (i < entry.getValue().size()) {
+                            if (i % 3 == 2) {
+                                distance = Math.pow(doubles.get(i), 2);
+                            }
+                        } else {
+                            if (i % 3 == 2) {
+                                distance = Math.pow(doubles.get(i) - entry.getValue().get(i), 2);
+                            }
+                        }
                     }
+
+                    System.out.println(Math.sqrt(distance));
                 }
+
             }
-            System.out.println(Math.sqrt(distance));
-        });
+        }
+    }
+
+}
 
             /*
 
@@ -38,7 +69,7 @@ public class JavaNearest {
              */
 
 
-        //System.out.println(radioMap.toString());
+//System.out.println(radioMap.toString());
 
 
 
@@ -73,7 +104,7 @@ public class JavaNearest {
          */
 
 
-    }
+
 
     /*
     public Map generateRadiomap() {
@@ -100,7 +131,6 @@ public class JavaNearest {
     }
 
      */
-}
 
 /*
 1. abc ændres til beaconid
